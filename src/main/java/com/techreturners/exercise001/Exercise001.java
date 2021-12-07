d6d5bf1b044b468b8fec054d31569be338a417a1
panthangiswapna <pspsowmya@gmail.com>
2021-12-07 01:15:47 +0000

    Solution for Exercise001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -4,27 +4,34 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        String capitalizedWord = Character.toUpperCase(word.charAt(0))+word.substring(1);
+        return capitalizedWord;
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        String initials = firstName.charAt(0)+"."+lastName.charAt(0);
+        return initials;
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+
+        double vat = Math.round((originalPrice + (vatRate/100)*originalPrice)*100.0)/100.0;
+        return vat;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        StringBuffer sb = new StringBuffer(sentence);
+        String revString = sb.reverse().toString();
+        return revString;
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int count = 0;
+        for(int i=0;i<=users.size()-1;i++) {
+            if (users.get(i).getType().equals("Linux")) {
+                count++;
+            }
+        }
+        return count;
     }
 }
